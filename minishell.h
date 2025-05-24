2db3d373a9b9b509ae5c9a2799ce25f296de3b45
O-A5M <akhmouchothman@gmail.com>
2025-05-24 01:07:32 +0100

    adding the simple command struct

--- minishell.h
+++ minishell.h
@@ -28,6 +28,22 @@ typedef struct s_export
 	struct s_export	*next;
 }				t_export;
 
+typedef struct	s_simple_command
+{
+	//which command should be executed.
+	char	*command_name;
+	//command arguments like -l, -rf, etc...
+	char	**args;
+	//variables assinged to the command line.
+	char	**var_assign;
+	//input redirection (<)
+	char	*red_in;
+	//output redirection (>>, >)
+	char	*red_out;
+	//flag for the input 0=> and 1=>>
+	int		out_mode;
+}				t_simple_command;
+
 void	set_signals(struct sigaction *sa_int, struct sigaction *sa_quit);
 void	parsing(void);
 void	sigaction_exit(void);
