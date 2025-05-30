bc4c4bed2bd0977c2b9230f01b637e5022570379
0obito <elmsafer.abdellah00@gmail.com>
2025-05-30 20:40:48 +0100

    Pushing today's work, made advancements but we're yet to be done

--- tokenizer.c
+++ tokenizer.c
@@ -82,9 +82,46 @@ size_t	count_tokens(char *cl)
 }
 */
 
-void	get_expanded_token(char *cl, int *idx, t_cmd **pipeline, t_args **args)
+int	is_a_whitespace(char c)
+{
+	if (a == ' ' || a >= 9 && a <= 13)
+		return (1);
+	else
+		return (0);
+}
+
+int	is_quote_character(char c)
+{
+	if (c == '\'' || c == '"')
+		return (1);
+	else
+		return (0);
+}
+
+/*
+	would have been fun but some will take time, it
+	will be inconvenient  to do some and not others
+
+/*int	is_a_special_variable(char c)
+{
+	if ((c >= '0' && c <= '9') || c == '#' || c == '?'
+		|| c == '$' || c == '-' || c == '*' || c == '@'
+		|| c == '_' || c == '!')
+		return (1);
+}
+
+void	print_special_variable(char c)
 {
 
+}*/
+
+void	print_exit_status(int *index)
+{
+	int	exit_status;
+
+	exit_status = 0;
+	*index += 1;
+	printf("%d\n", exit_status);
 }
 
 void	get_meta_token(char *cl, int *idx, t_cmd **pipeline)
@@ -94,7 +131,85 @@ void	get_meta_token(char *cl, int *idx, t_cmd **pipeline)
 
 void	get_normal_token(char *cl, int *idx, t_cmd **pipeline, t_args **args)
 {
-	
+	// You traverse, find the whole word, get it into a char *word.
+	// Then if you were stopped by a quote character or a `$` sign
+	//  you call the suitable function to get it, you get it in
+	//  another char *word, and you join it with the 1st word.
+	// And you keep going. Sounds like a great plan!
+	while (cl[*idx] && cl[*idx] != '\'' && cl[*idx] != '"'
+		&& cl[*idx] != '|' && cl[*idx] != '>' && cl[*idx] != '<'
+		&& cl[*idx] != ' ' && !(cl[*idx] >= 9 && cl[*idx] <= 13))
+	{
+		if (cl[*idx] == '$')
+			;
+		else
+
+	}
+}
+
+void	join_and_clean(char **result, char *pre_expansion, char *expanded_token)
+{
+	*result = ft_strjoin(*result, pre_expansion);
+	if (*result == NULL)
+		allocation_error();
+	*result = ft_strjoin(*result, expanded_token);
+	if (*result == NULL)
+		allocation_error();
+	free(pre_expansion);
+	free(expanded_token);
+}
+
+char	*get_expanded_token(char *cl, int *index/*, t_cmd **pipeline, t_args **args*/)
+{
+	char			*expanded_token;
+	unsigned int	start_index;
+	int				exit_status;
+
+	*index += 1;
+	start_index = *index;
+	if (is_a_whitespace(cl[*index]) || cl[*index] == '\0')
+		return (ft_strdup("$"));
+	else if (is_quote_character(cl[*index]))
+		return (ft_strdup(""));
+	else if (cl[*index] == '?')
+	{
+		*index += 1;
+		exit_status = 127;
+		return (ft_itoa(exit_status));
+	}
+	else if (is_a_valid_character(cl[*index]))
+	{
+
+	}
+
+char	*extract_expansions(char *quoted_text)
+{
+	char			*pre_expansion;
+	char			*expanded_token;
+	char			*result;
+	int				index;
+	unsigned int	start;
+
+	result = NULL;
+	index = 0;
+	while (quoted_text[index])
+	{
+		start = (unsigned int)index;
+		while (quoted_text[index] && quoted_text[index] != '$')
+			index++;
+		if (quoted_text[index])
+		{
+			if (!result)
+				result = "";
+			pre_expansion = ft_substr(quoted_text, start, index - start);
+			expanded_token = get_expanded_token(quoted_text, &index);
+			join_and_clean(&result, pre_expansion, expanded_token);
+		}
+	}
+	if (!result)
+		return (quoted_text);
+	free(quoted_text);
+	return (result);
 }
 
 char	*get_quoted_token(char *cl, int *index, t_cmd **pipeline, t_args **args)
@@ -107,12 +222,13 @@ char	*get_quoted_token(char *cl, int *index, t_cmd **pipeline, t_args **args)
 	*index += 1;
 	start_index = (unsigned int)*index;
 	while (cl[*index] != '\0' && cl[*index] != quote_character)
-	*index += 1;
+		*index += 1;
 	if (cl[*index] == '\0')
-	unclosed_quotes_error();
+		unclosed_quotes_error();
 	quoted_text = ft_substr(cl, start_index, (size_t)*index - (size_t)start_index);
 	*index += 1;
-	append_arg(args, quoted_text);
+	if (quote_character == '"')
+		quoted_text = extract_expansions(quoted_text);
 	return (quoted_text);
 }
 
@@ -126,7 +242,7 @@ void	get_tokens(char *cl, t_cmd **pipeline_head)
 	args = NULL;
 	while (cl[idx])
 	{
-		while (cl[idx] == ' ' || (cl[idx] >= 9 && cl[idx] <= 13))
+		while (is_a_whitespace(cl[idx]))
 			idx++;
 		if (cl[idx] == '\'' || cl[idx] == '"')
 			get_quoted_token(cl, &idx, pipeline, &args);
@@ -136,7 +252,7 @@ void	get_tokens(char *cl, t_cmd **pipeline_head)
 			get_expanded_token(cl, &idx, pipeline, &args);
 		if (cl[idx] != '\'' && cl[idx] != '"' && cl[idx] != '|'
 			&& cl[idx] != '>' && cl[idx] != '<' && cl[idx] != '$'
-			&& cl[idx] != ' ' && !(cl[idx] >= 9 && cl[idx] <= 13))
+			&& !(is_a_whitespace(cl[idx])))
 			get_normal_token(cl, &idx, pipeline, &args);
 	}
 }
