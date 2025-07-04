9a13a3a89798a6fdd401f2f3b72c591e4b1ce60a
O-A5M <akhmouchothman@gmail.com>
2025-07-04 15:46:11 +0100

    test

--- execution/source/execution.c
+++ execution/source/execution.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oakhmouc <oakhmouc@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 18:53:51 by oakhmouc          #+#    #+#             */
+/*   Updated: 2025/07/04 15:45:46 by oakhmouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/execution.h"
 #include "../../minishell.h"
 
 void  start_execution(t_cmd *cmd, char **env)
 {
-	execve(cmd->args_array[0], cmd->args_array, env);
+
 }
 
